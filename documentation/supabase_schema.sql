b6cb64040557bd4df5be2773cf3c161acc130d6a
Lenneth Rosario <s4036183@student.rmit.edu.au>
2026-04-14 18:52:03 +1000

    Delete documentation/supabase_schema.sql

--- documentation/supabase_schema.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE observations (
-    id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
-    species TEXT NOT NULL,
-    confidence FLOAT4 NOT NULL,
-    recorded_at TIMESTAMPTZ DEFAULT NOW(),
-    location JSONB
-);
