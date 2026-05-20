173b9789634291045a6f0c64ad69caac6c0ef937
Bridget Valder <Bridget.Valder@gmail.com>
2026-05-21 05:36:03 +1000

    preparing for PR

--- FrogFinder/screens/AboutScreen.jsx
+++ FrogFinder/screens/AboutScreen.jsx
@@ -6,7 +6,7 @@ export const AboutScreen = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>About FrogFinder</Text>
-      <Text>Description of the application.</Text>
+      <Text>This application is designed to assist enthusiasts and researchers with identifying frog species found in Victoria. Simply record or upload an audio file of the frog call and receive a real-time assessment of the most likely frog type.</Text>
     </View>
   );
 };
@@ -15,6 +15,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: Theme.spacing.md,
+    paddingTop: 60,
   },
   title: {
     ...Theme.typography.h2,
